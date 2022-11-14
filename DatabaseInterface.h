ba362851fccfa128a72377700d4387f084bfdc92
Pedro <pedrocorreiabatista@gmail.com>
2022-11-14 15:58:17 +0100

    not fini

--- DatabaseInterface.h
+++ DatabaseInterface.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Composer.h"
 using namespace std;
 namespace DatabaseInterface {
 	inline void printMenu() {
@@ -9,15 +10,28 @@ namespace DatabaseInterface {
 			<< "4) List all composers" << endl
 			<< "5) List all composers by rank" << endl
 			<< "0) Quit" << endl
-			<< "******************************************************";
+			<< "*****************************************************" << endl;
 	}
 
-	void printAddingComposerMenu() {
+	inline void printAddingComposerMenu() {
 		cout << "*****************************************************" << endl
 			<< "1) Add a new composer" << endl
 			<< "2) Add a new composer front" << endl
 			<< "3) Add a new composer at" << endl
 			<< "0) Back" << endl
-			<< "******************************************************";
+			<< "*****************************************************" << endl;
+	}
+
+	inline Composer& createComposer() {
+		string firstName, lastName, composerGenre, fact;
+		int yearOfBirth, ranking;
+		cout << "*****************************************************" << endl
+			<< "First name: ", cin >> firstName, cout << endl << "Last name: ", cin >> lastName, cout << endl
+			<< "Year of birth: ", cin >> yearOfBirth, cout << endl
+			<< "Genre: ", cin >> composerGenre, cout << endl << "Fact: ", cin >> fact, cout << endl
+			<< "Ranking: ", cin >> ranking, cout << endl
+			<< "*****************************************************" << endl;
+		Composer comp(firstName, lastName, yearOfBirth, composerGenre, fact, ranking);
+		return comp;
 	}
 }
--- Main.cpp
+++ Main.cpp
@@ -4,27 +4,36 @@
 #include <iostream>
 #include <string>
 
-void handleAddingComposermenu() {
+Database database;
+
+void handleAddingComposerMenu() {
 	DatabaseInterface::printAddingComposerMenu();
 	int response = 1;
-	while (response != 0) {
-		cin >> response;
-		if (response == 1) { 
-			Database database;
-			database.addComposer_back();
-		}
+	cin >> response; 
+	if (response == 0) return;
+	if (response == 1) { 
+		database.addComposer_back(DatabaseInterface::createComposer());
+		return;
 	}
+	if (response == 2) {
+		database.addComposer_front(DatabaseInterface::createComposer());
+		return;
+	}
+	int index;
+	cout << endl << "What index? ", cin >> index, cout << endl;
+	database.addComposer_at(DatabaseInterface::createComposer(), index);
 }
 
-void handleMenuChoises() {
+void handleMenuChoices() {
 	int response = 1;
 	while (response != 0) {
 		cin >> response;
-		if (response == 1) { handleAddingComposermenu(); }
+		if (response == 1) { handleAddingComposerMenu(); }
 	}
 }
 
 int main() {
 	DatabaseInterface::printMenu();
+	handleMenuChoices();
 	return 0;
 }
